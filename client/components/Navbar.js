98277bad055b7dd8e24d67f7c52aeb4f0251c0d0
PuravParab <47409476+PuravParab@users.noreply.github.com>
2023-04-04 23:51:21 +0400

    navbar update

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,13 +1,20 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const Navbar = () => {
+	const router = useRouter()
+
+	const isActiveLink = (page) => {
+		return router.pathname === page? styles.active: ""
+	}
+
 	return (
 		<div className={styles.navbar}>
 			{/* <Link href='/listfy'>Listfy</Link> */}
-			<Link href='/'>Top</Link>
+			<Link href='/' className={isActiveLink('/')}>Top</Link>
 			{/* <Link href='/iceberg'>Iceberg</Link> */}
-			<Link href='/circle'>Inner circle</Link>
+			<Link href='/circle' className={isActiveLink('/circle')}>Inner circle</Link>
 			{/* <Link href='/other'>Templates</Link> */}
 		</div>
 	)
