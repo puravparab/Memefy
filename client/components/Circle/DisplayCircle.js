5dee30846b835363ef157b6d9ffe0e48646e5862
PuravParab <47409476+PuravParab@users.noreply.github.com>
2023-04-07 21:49:05 +0400

    fixed issues with circle generation

--- client/components/Circle/DisplayCircle.js
+++ client/components/Circle/DisplayCircle.js
@@ -16,16 +16,23 @@ const DisplayCircle = ( props ) => {
 	const [ring3, setRing3] = useState('')
 
 	const ref = useRef(null);
+	// Holds url for generated image
 	const [imageDataUrl, setImageDataUrl] = useState('')
+	// Is a new image generated
+	const [imageGenerated, setImageGenerated] = useState(false) 
 
 	useEffect(()=>{
 		populateRings()
 	},[numArtists])
 
+	// Re-render when users changes no of rings
 	useEffect(()=>{
 		setRings(props.rings)
+		setImageDataUrl('')
+		setImageGenerated(false)
 	},[props.rings])
 
+	// Populate the artists in the circle
 	const populateRings = () => {
 		setRing1(props.artist_list.map((img_src, id) =>{
 			let classes = [
@@ -34,7 +41,8 @@ const DisplayCircle = ( props ) => {
 			if (id < 6){
 				return (
 					<div className={classes[id]} key={id}>
-						<Image src={img_src} width={110} height={110} alt={"artist " + id} />
+						{/* <Image src={img_src} width={110} height={110} alt={"artist " + id} /> */}
+						<img src={img_src} alt={"artist " + id} />
 					</div>
 				)
 			}
@@ -48,7 +56,8 @@ const DisplayCircle = ( props ) => {
 				if (id >= 6 && id < 18){
 					return (
 						<div className={classes[id - 6]} key={id}>
-							<Image src={img_src} width={90} height={90} alt={"artist " + id}/>
+							{/* <Image src={img_src} width={90} height={90} alt={"artist " + id} /> */}
+							<img src={img_src} alt={"artist " + id} />
 						</div>
 					)
 				}
@@ -63,60 +72,77 @@ const DisplayCircle = ( props ) => {
 					if (id >= 18 && id < 30){
 						return (
 							<div className={classes[id - 18]} key={id}>
-								<Image src={img_src} width={80} height={80} alt={"artist " + id}/>
+								{/* <Image src={img_src} width={80} height={80} alt={"artist " + id} /> */}
+								<img src={img_src} alt={"artist " + id} />
 							</div>
 						)
 					}
 				}))
 			}
 		}
+		setImageDataUrl('')
+		setImageGenerated(false)
 	}
 
-	// Convert to png
-	const handleDownload = () => {
+	// Convert to JPEG
+	const handleImageGeneration = () => {
 		if (ref.current === null) {
 			return
 		}
-		// var vp = document.getElementById("viewportMeta").getAttribute("content")
-		// document.getElementById("viewportMeta").setAttribute("content", "width=1920")
-		// window.scrollTo(0, 0)
-		// const dpr = window.devicePixelRatio
-		// console.log("dpr", dpr)
-// 		const element = ref.current;
-// 		const width = element.offsetWidth;
-// 		const height = element.offsetHeight;
-// 		// 
-// 
-// 		html2canvas(ref.current, {width, height})
-// 			.then((canvas) => {
-// 				console.log(canvas)
-// 				document.body.appendChild(canvas);
-// 				console.log(window.outerWidth)
-// 				console.log(window.outerHeight)
-// 				const dataURI = canvas.toDataURL('image/jpeg')
-// 				setImageDataUrl(dataURI)
-// 				// const link = document.createElement('a');
-// 				// link.download = 'memefy-circle.png';
-// 				// link.href = canvas.toDataURL('image/png');
-// 				// link.click();
-// 				// document.getElementById("viewportMeta").setAttribute("content", vp);
-// 			})
+		// Convert div to JPEG using htmlToImage
 		let vp = document.getElementById("viewportMeta").getAttribute("content")
 		document.getElementById("viewportMeta").setAttribute("content", "width=1920")
-
-		htmlToImage.toPng(ref.current)
+		htmlToImage.toJpeg(ref.current)
 			.then(function (dataUrl) {
 				setImageDataUrl(dataUrl)
 				document.getElementById("viewportMeta").setAttribute("content", vp);
 				// Clean up any canvas elements added to the DOM
-			  const canvasElements = document.getElementsByTagName('canvas');
-			  Array.from(canvasElements).forEach(canvasElement => {
-			  	console.log("ASd")
-			    canvasElement.remove();
-			  })
+				const canvasElements = document.getElementsByTagName('canvas')
+				Array.from(canvasElements).forEach(canvasElement => {
+					canvasElement.remove()
+				})
+				setImageGenerated(true)
 			})
 			.catch(function (error) {
-				console.error('oops, something went wrong!', error);
+				console.error('oops, something went wrong!', error)
+				setImageGenerated(false)
+			});
+	}
+
+	// Download generated images
+	const handleDownload = () => {
+		const link = document.createElement('a');
+		link.download = 'memefy-circle.png';
+		link.href = imageDataUrl
+		link.click();
+	}
+
+	// Convert to JPEG and download
+	const handleDownloadComplete = () => {
+		if (ref.current === null) {
+			return
+		}
+		// Convert div to JPEG using htmlToImage
+		let vp = document.getElementById("viewportMeta").getAttribute("content")
+		document.getElementById("viewportMeta").setAttribute("content", "width=1920")
+		htmlToImage.toJpeg(ref.current)
+			.then(function (dataUrl) {
+				setImageDataUrl(dataUrl)
+				document.getElementById("viewportMeta").setAttribute("content", vp);
+				// Clean up any canvas elements added to the DOM
+				const canvasElements = document.getElementsByTagName('canvas')
+				Array.from(canvasElements).forEach(canvasElement => {
+					canvasElement.remove()
+				})
+				setImageGenerated(true)
+				const link = document.createElement('a');
+				link.download = 'memefy-circle.png';
+				link.href = dataUrl
+				link.click();
+			})
+			.catch(function (error) {
+				console.error('oops, something went wrong!', error)
+				setImageGenerated(false)
 			});
 	}
 
@@ -133,7 +159,8 @@ const DisplayCircle = ( props ) => {
 						<div className={styles.innerRing}>
 							{ring1}
 							<div className={styles.ringCenter}>
-								<Image src={props.artist_list[0]} width={120} height={120} alt="user's image"/>
+								{/* <Image src={props.artist_list[0]} width={120} height={120} alt="user's image"/> */}
+								<img src={props.artist_list[0]} alt="user's image" />
 							</div>
 						</div>
 					}
@@ -145,7 +172,8 @@ const DisplayCircle = ( props ) => {
 							<div className={styles.innerRing}>
 								{ring1}
 								<div className={styles.ringCenter}>
-									<Image src={props.artist_list[0]} width={120} height={120} alt="user's image"/>
+									{/* <Image src={props.artist_list[0]} width={120} height={120} alt="user's image"/> */}
+									<img src={props.artist_list[0]} alt="user's image" />
 								</div>
 							</div>
 						</>
@@ -161,7 +189,8 @@ const DisplayCircle = ( props ) => {
 							<div className={styles.innerRing}>
 								{ring1}
 								<div className={styles.ringCenter}>
-									<Image src={props.artist_list[0]} width={120} height={120} alt="user's image"/>
+									{/* <Image src={props.artist_list[0]} width={120} height={120} alt="user's image"/> */}
+									<img src={props.artist_list[0]} alt="user's image" />
 								</div>
 							</div>
 						</>
@@ -169,14 +198,17 @@ const DisplayCircle = ( props ) => {
 				</div>
 
 				<div className={styles.displayCircleFooter}>
-					<Image src={Spotify} width={133} height={40} alt="spotify logo"/>
+					<Image src={Spotify} width={133} height={40} alt="spotify logo" />
 					<h4>memefy.app/circle</h4>
 				</div>
 			</div>
 
-			<img src={imageDataUrl} className={styles.displayCircleImage} />
-			<div>
-				<button onClick={handleDownload}>Download as PNG</button>
+			{imageDataUrl && <img src={imageDataUrl} className={styles.displayCircleImage} alt="user's inner circle"/>}
+
+			<div className={styles.circleBtnContainer}>
+				{!imageGenerated && <button onClick={handleImageGeneration} className={styles.generateBtn}>Generate</button>}
+				{imageGenerated && <button onClick={handleDownload} className={styles.downloadSmallBtn}>Download</button>}
+				<button onClick={handleDownloadComplete} className={styles.downloadLargeBtn}>Download</button>
 			</div>
 		</>
 	)
